eb26cd1c144ba3ae95310c729db0f5a04bcbca1f
Chantelle Gilmer <77417385+MasterEngineer49@users.noreply.github.com>
2022-02-23 19:44:38 -0600

    My first commit for this assignment

---
+++ main.py
@@ -0,0 +1,68 @@
+# This Program is a Word Translation Project
+
+from textblob import TextBlob
+
+def wordtranslation():
+    print("Spanish")
+
+def wordtranslation():
+    print("French")
+
+def translatewords():
+    print("Words into Spanish")
+    print("Spanish")
+    print("Words into French")
+    print("French")
+
+def onetranslatewords():
+    print("Translate words into Spanish")
+    words = TextBlob(input("Enter English word or phrase"))
+    # input variable for Spanish from TextBlob translation
+    span_words = TextBlob
+    # determines the sentiment of words
+    print(words.translate(to="es"))
+
+def twotranslatewords():
+    print("Translate word into French")
+    words = TextBlob(input("Enter English word or phrase"))
+    # input variable for French from TextBlob translation
+    french_words = TextBlob
+    # determines the sentiment of words
+    print(words.translate(to="fr"))
+
+def threetranslatewords(str1):
+    print("string = str1")
+
+def loops():
+    inputNum = int(input("please enter a number for the loop"))
+    while inputNum >0:
+        print(inputNum)
+        print("loop")
+        p = input("pause")
+        inputNum = inputNum-1
+
+
+
+def main():
+    onetranslatewords()
+    number = twotranslatewords()
+    print("translate = ")
+    loops()
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
